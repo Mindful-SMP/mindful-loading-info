4e23b52c3496935331bac02059fca813ea34f7b6
Iautner <Lautnerbusiness@gmail.com>
2024-04-17 06:06:04 -0700

    Create WindowOpenListener.java

---
+++ remappedSrc/com/lautner/mindful_loading_window/WindowOpenListener.java
@@ -0,0 +1,18 @@
+package com.lautner.mindful_loading_window;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public final class WindowOpenListener {
+    private static final List<Runnable> listeners = new LinkedList<>();
+    
+    public static List<Runnable> getListeners() {
+        return listeners;
+    }
+
+    public static void trigger() {
+        for (var l : listeners) {
+            l.run();
+        }
+    }
+}
